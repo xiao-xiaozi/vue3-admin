43348fe4330fb8a7581182ce52b6e4a0894d95e3
小小子 <xiaoxiaozi_qiu@163.com>
2022-09-28 17:54:22 +0800

    feat:引入Element-plus，并添加自动按需导入组件配置
    1. 引入sass依赖

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // element-plus 按需自动引入组件
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    // element-plus 按需自动引入组件
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  }
 });
