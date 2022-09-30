6e2ed57a95f85d6e9adf21e913a45cf668c85d49
小小子 <xiaoxiaozi_qiu@163.com>
2022-09-30 11:45:51 +0800

    feat: 将侧边菜单的数据修改为当前选中菜单的顶层菜单的子级菜单

--- src/Layout/AsideMenu.vue
+++ src/Layout/AsideMenu.vue
@@ -3,14 +3,24 @@ import SubMenu from "./SubMenu.vue";
 import { useMenuStore } from "../stores/menu";
 import { computed } from "vue";
 const menuStore = useMenuStore();
-const menus = computed(() => menuStore.menus);
+const menus = computed(() => menuStore.asideMenu);
 </script>
 <template>
-  <el-menu :default-active="menuStore.currentMenu" :router="true">
+  <el-menu
+    :default-active="menuStore.currentMenu"
+    :router="true"
+    v-if="menus.length">
     <template v-for="menu in menus" :key="menu.path">
       <SubMenu v-if="menu.children" :menu="menu" />
       <el-menu-item v-else :index="menu.path">{{ menu.title }}</el-menu-item>
     </template>
   </el-menu>
+  <div v-else class="not-child-menu">暂无子级菜单!</div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.not-child-menu {
+  color: #333;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
--- src/stores/menu.js
+++ src/stores/menu.js
@@ -1,15 +1,27 @@
-// import { ref, computed } from "vue";
-import { reactive, ref } from "vue";
+import { computed, reactive, ref } from "vue";
 import { defineStore } from "pinia";
 import menuData from "@/mock/menu.js";
 
 export const useMenuStore = defineStore("menu", () => {
   let currentMenu = ref("/index");
+  // 头部菜单
   const menus = reactive(menuData);
-  // const doubleCount = computed(() => count.value * 2);
+  // 侧边菜单,
+  const asideMenu = computed(() => {
+    let currentMenuComprise = currentMenu.value.split("/");
+    let topMenuComprise = currentMenuComprise[1];
+    let arr = [];
+    for (let item of menus) {
+      if (item.path.includes(topMenuComprise)) {
+        arr = item.children ? item.children : [];
+        break;
+      }
+    }
+    return arr;
+  });
   function setCurrentMenu(value) {
     currentMenu.value = value;
   }
 
-  return { currentMenu, menus, setCurrentMenu };
+  return { currentMenu, menus, asideMenu, setCurrentMenu };
 });
