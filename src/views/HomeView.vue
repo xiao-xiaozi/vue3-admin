769f8f606f07c32bee0825dc9495e7255b48c87e
xiao-xiaozi <xiao-xiaozi@outlook.com>
2025-05-14 15:49:09 +0800

    feat: 首页加encode/decode和base64加解密

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,59 @@
 <script setup>
-import { onMounted } from "vue"
-// import { api } from "@/api"
+import { ref } from 'vue';
 
-onMounted(() => {
-//   console.log(api.mockGet())
-  // api.userResourceGet().then(res => {
-  //   console.log(res)
-  // })
-})
+const enOrDeCode = ref('')
+const enOrDeCodeResult =ref('')
+function encodeFn(){
+  enOrDeCodeResult.value = encodeURIComponent(enOrDeCode.value)
+}
+
+function decodeFn(){
+  enOrDeCodeResult.value = decodeURIComponent(enOrDeCode.value)
+}
+
+const val = ref('')
+const valResult = ref('')
+
+function atobFn() {
+  valResult.value = atob(val.value)
+}
+
+function btoaFn() {
+  valResult.value = btoa(val.value)
+}
 
 </script>
 
 <template>
-  <div>Main</div>
+  <div>
+    <div>
+      <el-divider>encode or decode</el-divider>
+      <el-input v-model="enOrDeCode" />
+      <div class="encode-decode">
+        <el-button type="primary" @click="encodeFn">encode</el-button>
+        <el-button type="primary" @click="decodeFn">decode</el-button>
+      </div>
+      <el-input v-model="enOrDeCodeResult" readonly />
+    </div>
+    <div class="function-box">
+      <el-divider>atob or btoa</el-divider>
+
+      <el-input v-model="val" />
+      <div class="encode-decode">
+        <el-button type="primary" @click="atobFn">atob</el-button>
+        <el-button type="primary" @click="btoaFn">btoa</el-button>
+      </div>
+      <el-input v-model="valResult" readonly />
+    </div>
+  </div>
 </template>
+<style lang="scss" scoped>
+.encode-decode {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.function-box {
+  margin-top: 40px;
+}
+</style>
